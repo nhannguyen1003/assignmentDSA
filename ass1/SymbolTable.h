427e3c08cdaa8a181c0cb1a0eb66127d71303d69
nhannguyen1003 <0046isk@gmail.com>
2021-08-30 18:42:18 -0400

    update symbol.h

--- ass1/SymbolTable.h
+++ ass1/SymbolTable.h
@@ -8,7 +8,44 @@ public:
     SymbolTable() {}
     void run(string filename);
 };
-class Node{
-    
+struct Information
+{
+    string name;
+    string type;
+    int scope;
+    Information(string name) :name(name), type("number")
+    {
+        this->scope=LinkedList::getScope();
+    }
+    Information(string name,string type) :name(name), type(type)
+    {
+        this->scope=LinkedList::getScope();
+    } 
 };
+
+class Node{
+    public:
+    Information data;
+    Node* next;
+    Node(Information data,Node* next);
+    Node(Information data);
+};
+
+class LinkedList{
+    Node* head;
+    static int spn;
+    public:
+    LinkedList() :head(nullptr) {}
+    LinkedList(Node* head) :head(head) {}
+    static int getScope()
+    {
+        return spn;
+    }
+    Node* getHead();
+    void setHead(Node* head);
+};
+
+
+bool isValidLine(LinkedList* LL,string s);
+
 #endif
